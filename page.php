a10bd165beb5a1c453cb1dcb212c886e1759ed5b
soklope <skp2104@hotmail.com>
2025-09-09 19:49:30 +0200

    Add child/parent logic for page header

--- page.php
+++ page.php
@@ -1,12 +1,30 @@
 <?php get_header(); ?>
 
+<?php $parent_id = wp_get_post_parent_id( get_the_ID() );?>
+
 <main id="main" class="site-main">
     <?php if ( ! is_front_page() ) : ?>
-        <div class="page-heading page-container">
-            <h1 class="page-heading__title"><?php the_title(); ?></h1>
-            <hr>
-            <p>X</p>
-        </div>
+        <?php if ( $parent_id ) : ?>
+
+            <div class="page-heading page-container">
+                <h1 class="page-heading__title">
+                    <?php echo get_the_title( $parent_id ); ?>
+                </h1>
+                <hr>
+                <h3 class="page-heading__sub-title"><?php the_title(); ?></h3>
+            </div>
+            
+            <?php else : ?>
+                
+                <div class="page-heading page-container">
+                    <h1 class="page-heading__title"><?php the_title(); ?></h1>
+                    <hr>
+                    <span class="page-heading__icon"></span>
+                </div>
+
+        <?php endif; ?>
+
+        
     <?php endif; ?>
 
     <?php 
