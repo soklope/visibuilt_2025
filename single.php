8583e0315554b7e72e859f6dc2e48e148075607e
soklope <skp2104@hotmail.com>
2025-09-21 17:34:28 +0200

    Add styling for block query

--- single.php
+++ single.php
@@ -1,13 +1,21 @@
 <?php get_header(); ?>
 
 <main id="main" class="site-main">
-    <?php 
-        if ( have_posts() ) : 
-            while ( have_posts() ) : the_post(); 
-                the_content();
-            endwhile; 
-        endif; 
-    ?>
+    <?php if ( have_posts() ) : ?>
+        <?php while ( have_posts() ) : the_post(); ?>
+            <div class="page-container">
+                <p class="single__date"><?php echo get_the_date(); ?></p>
+                <h2 class="single__title"><?php the_title(); ?></h2>
+                <div class="single__hero">
+                    <?php the_post_thumbnail( 'full' ); ?>
+                </div>
+                
+                <div class="single__content">
+                    <?php the_content(); ?>
+                </div>
+            </div>
+        <?php endwhile; ?>
+    <?php endif; ?>
 </main>
 
 <?php get_footer(); ?>
